7078174306b249713d505633503ecaf3f61b1138
mickson-ui <mbonsu69@outlook.com>
2025-10-19 14:41:37 +0200

    update api endpoint

--- frontend/sneaker-frontend/src/components/custom-ui/upload.tsx
+++ frontend/sneaker-frontend/src/components/custom-ui/upload.tsx
@@ -35,7 +35,7 @@ export function Upload(){
         setLoading(true)
 
         try{
-            const res = await fetch("http://localhost:8000/predict", {
+            const res = await fetch("https://sneaker-ai-backend.onrender.com/predict", {
                 method: "POST",
                 body: formData,
         })
